4a3bbb6222afac2fc38ea124ec8ac517aea05092
Michail Kapustin <peppakat@mail.ru>
2020-10-24 04:35:55 +0500

    SubArray

---
+++ Array'sJob/Array'sJob/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Array_sJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void InitializeArrays()
+        {
+
+        }
+    }
+}
