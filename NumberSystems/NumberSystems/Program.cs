c2cfe47a9ee12d631a43e945f9ae8972813cd3e4
Michail Kapustin <peppakat@mail.ru>
2020-11-02 22:05:42 +0500

    Revert "done"
    This reverts commit 1d9d324078fdc050caadbd8dfbbe31156c81a8cb.

--- NumberSystems/NumberSystems/Program.cs
+++ NumberSystems/NumberSystems/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Reflection;
 
 namespace NumberSystems
 {
@@ -9,28 +7,25 @@ namespace NumberSystems
     {
         static void Main(string[] args)
         {
-            string input = InsertTheNumber();
-            string[] reversedCount = new string[input.Length];
-            for (int z = 0; z < input.Length; z++)
-                reversedCount[input.Length - z - 1] = Convert.ToString(input[z]);
 
+            var count = InsertTheNumber();
             var countBase = InsertOriginalSystem();
             var convertedBase = InsertConvertedSystem();
+            int[] powArray = new int[Convert.ToString(count).Length];
             Console.Clear();
 
-            PrintTheResult(reversedCount, countBase, convertedBase, input);
+            PrintTheResult(count, countBase, powArray, convertedBase);
             Console.ReadKey();
         }
 
-
-        //1) перевод из любой в любую систему счисления но я не стал добавлять после конца латинского алфавита символы ( на мой взгляд в программе это можно встроить одним движением примерно в 57 строке)
-        //                                                                
+        //1) перевод в любую систему счисления но к сожалению без A,B... (не успел допилить + вышло нагромождение принтов из-за просьбы "для пятиклассника" +
+        //                                                                 + из-за них стало тяжело читать код а как отдельно их сделать я не придумал)
         //2) перевод в римские 
 
-        static string InsertTheNumber()
+        static int InsertTheNumber()
         {
             Console.WriteLine("Enter the number");
-            return Console.ReadLine(); 
+            return int.Parse(Console.ReadLine()); 
         }
 
         static int InsertOriginalSystem()
@@ -44,62 +39,57 @@ namespace NumberSystems
             Console.WriteLine("Enter the converted number's base");
             return int.Parse(Console.ReadLine());
         }
-
-        static string[] SetPowArray(string[] reversedCount)                                           
+                                                                                                          //   powArray[index](further pow)          =    0 1 2     
+        static int[] SetToPowArray(int count,int[] powArray)                                         // perform the count as in ex.: number 135  ---->    5 3 1   
         {
-            for (int i = 0; i < reversedCount.Length; i++)
+            var temp = count;
+
+            while (temp != 0)
             {
-                try
+                for (int i = 0; i < Convert.ToString(count).Length; i++)
                 {
-                    Convert.ToInt32(reversedCount[i]);
+                    powArray[i] = temp % 10;
+                    Console.WriteLine($"Index ={i} numeral ={powArray[i]}");
+                    temp /= 10;
                 }
-                catch
-                {
-                    for (char s = 'A'; s <= 'Z'; s++)
-                        for (int index = 10; index < 26; index++)
-                            if (reversedCount[i] == Convert.ToString(s))
-                                reversedCount[i] = Convert.ToString(index);
-                }
-                Console.WriteLine($"Index ={i} numeral ={reversedCount[i]}");
-            }
-            return reversedCount;
+            } 
+
+            return powArray;
         }
 
-        static int CalculateDecimal(int countBase,string[] reversedCount)
+        static int CalculateDecimal(int countBase, int[] powArray)
         {
             var decimalCount = 0;
 
-            for (int i = 0; i < reversedCount.Length; i++)
+            for (int i = 0; i < powArray.Length; i++)
             {
-                decimalCount += Convert.ToInt32(reversedCount[i]) * (int)Math.Pow(countBase, i);
-                Console.WriteLine($"{reversedCount[i]} * ({countBase} ^ {i}) = {decimalCount} ");
+                decimalCount += powArray[i] * (int)Math.Pow(countBase, i);
+                Console.WriteLine($"{powArray[i]} * ({countBase} ^ {i}) = {decimalCount} ");
             }
             Console.WriteLine();
             Console.WriteLine("Now we got the decimal value!");
             Console.WriteLine();
-
             return decimalCount;
         }
 
-        static void PrintTheResult(string[] reversedCount,int countBase, int convertedBase,string input)
+        static void PrintTheResult(int count,int countBase,int[] powArray,int convertedBase)
         {
-            Console.WriteLine($"We need to perform number: {input} which base is {countBase} into the number with base equal to: {convertedBase}");
+            Console.WriteLine($"We need to perform number: {count} which base is {countBase} into the number with base equal to: {convertedBase}");
             Console.WriteLine();
-            Console.WriteLine($"Our first step is to build an inverted format (where the indexes from 0 to {Convert.ToString(reversedCount).Length - 1} are equal to the pows)");
+            Console.WriteLine($"Our first step is to build an inverted format (where the indexes from 0 to {Convert.ToString(count).Length - 1} are equal to the pows)");
             Console.WriteLine();
-            SetPowArray(reversedCount);
+            SetToPowArray(count, powArray);
             Console.WriteLine();
             Console.WriteLine($"The next step is to multiply each element of the array by the base = {countBase} extented to power of each index and summ the multiplies");
             Console.WriteLine();
-            Console.WriteLine(CalculateConverted(convertedBase, countBase, reversedCount));
+            Console.WriteLine(CalculateConverted(convertedBase, countBase, powArray));
             Console.WriteLine();
         }
 
-        static string CalculateConverted(int convertedBase, int countBase, string[] reversedCount)
+        static string CalculateConverted(int convertedBase,int countBase,int[] powArray)
         {
-            var tempDecimal = CalculateDecimal(countBase, reversedCount);
+            var tempDecimal = CalculateDecimal(countBase, powArray);
             string convertedCount = null;
-            
 
             if (convertedBase == 10)
                 return convertedCount;
@@ -109,47 +99,21 @@ namespace NumberSystems
                 Console.WriteLine();
                 Console.WriteLine($"We must divide the decimal number by {convertedBase} and write the rests from the the last decimal_number/base to the first rest");
                 Console.WriteLine();
-                int index = 10;
-                char s = 'A';
 
                 while (tempDecimal > convertedBase)
                 {
                     Console.WriteLine($"{tempDecimal} % {convertedBase} = {tempDecimal % convertedBase}");
                     Console.WriteLine();
 
-                    if (tempDecimal % convertedBase > 9)
-                    {
-                        while (tempDecimal % convertedBase != index) 
-                        {
-                            index ++;
-                            s ++;
-                        } 
-
-                        convertedCount += s;
-                    }
-                    else convertedCount += Convert.ToString(tempDecimal % convertedBase);
-
-                    tempDecimal /= convertedBase;
+                    convertedCount += Convert.ToString(tempDecimal % convertedBase);
+                    tempDecimal = tempDecimal / convertedBase;
                 }
 
-                int indexLast = 10;
-                char sLast = 'A';
+                convertedCount += Convert.ToString(tempDecimal % convertedBase);
 
-                if (tempDecimal > 9)
-                {
-                    while(tempDecimal != indexLast)
-                    {
-                        indexLast++;
-                        sLast++;
-                    }
-
-                    convertedCount += sLast;
-                }
-                else convertedCount += Convert.ToString(tempDecimal);
-
-                char[] temporary = convertedCount.ToCharArray();
-                Array.Reverse(temporary);
-                return new string(temporary);  
+                char[] reversedCount = convertedCount.ToCharArray();
+                Array.Reverse(reversedCount);
+                return new string(reversedCount);  
             }         
         }
     }
