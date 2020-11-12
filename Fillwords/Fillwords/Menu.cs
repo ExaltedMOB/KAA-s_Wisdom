c52626a3aaa4dca6956082c7d1c2adcd739d0d2c
Michail Kapustin <peppakat@mail.ru>
2020-11-13 00:26:27 +0500

    task1-menu-screen

---
+++ Fillwords/Fillwords/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Fillwords
+{
+    public class Menu
+    {
+        public string[] buttons;
+
+        public static Menu[] common;
+
+        public static void PrintTheTitle()
+        {
+            Console.SetWindowSize(80, 30);
+            Console.SetCursorPosition(30, 3);
+            Console.WriteLine("<<FILLWORDS>>");
+        }
+
+        public static void FillTheButtons()
+        {
+            common = new[]
+            {
+                new Menu
+                {
+                    buttons = new string[4]{
+                        "New Game",
+                        "Resume",
+                        "High Score",
+                        "Exit"}
+                }
+            };
+        }
+
+        public static void PrintTheButtons()
+        {
+            var k = 5;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(32, k);
+                Console.WriteLine(common[0].buttons[i]);
+                k += 2;
+            }
+        }
+    }
+}
--- Fillwords/Fillwords/Program.cs
+++ Fillwords/Fillwords/Program.cs
@@ -4,9 +4,47 @@ namespace Fillwords
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        //public static Buttons[] common;
 
+        public static void Main(string[] args)
+        {
+            Menu.PrintTheTitle();
+            Menu.FillTheButtons();
+            Menu.PrintTheButtons();
+            Console.ReadKey();
         }
+
+        //static void PrintTheTitle()
+        //{
+        //    Console.SetWindowSize(80, 30);
+        //    Console.SetCursorPosition(30, 3);
+        //    Console.WriteLine("<<FILLWORDS>>");
+        //}
+
+        //static void FillTheButtons()
+        //{
+        //    common = new[]
+        //    {
+        //        new Buttons
+        //        {
+        //            buttons = new string[4]{
+        //                "New Game",
+        //                "Resume",
+        //                "High Score",
+        //                "Exit"}
+        //        }
+        //    };
+        //}
+
+        //static void PrintTheButtons()
+        //{
+        //    var k = 5;
+        //    for (int i = 0; i < 4; i++)
+        //    {
+        //        Console.SetCursorPosition(32, k);
+        //        Console.WriteLine(common[0].buttons[i]);
+        //        k+=2;
+        //    }
+        //}
     }
 }
