0d4c0fa9210d45f6b00e7584695810bd970e856c
Michail Kapustin <peppakat@mail.ru>
2020-11-29 21:57:44 +0500

    ready to push

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,23 +10,34 @@ namespace Calculator
         static void Main(string[] args)
         {
             //Console.WriteLine(Directory.GetCurrentDirectory());
-            string[] lines = File.ReadAllLines("input.txt");
+            string[] lines = ReadTheFile();
 
-            string newLine = "";
-            lines = DeleteSpaces(lines, newLine);
+            lines = DeleteSpaces(lines);
 
             string[] dividedLine = new string[3];
-            string[] output = new string[lines.Length];
-            output = DivideTheExpression(dividedLine, lines, output);
 
-            File.WriteAllLines("output.txt", output);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string result = "";
+                DivideTheExpression(dividedLine, lines, i);
+                lines[i] = CalculateTheLine(dividedLine[0], dividedLine[1], dividedLine[2], result);
+            }
+
+            File.WriteAllLines("output.txt", lines);
         }
 
-        static string[] DeleteSpaces(string[] lines, string newLine)
+        static string[] ReadTheFile()
+        {
+            return File.ReadAllLines("input.txt");
+        }
+
+        static string[] DeleteSpaces(string[] lines)
         {
             for(int s = 0; s < lines.Length; s++)
             {
-                for(int i = 0; i < lines[s].Length; i++)
+                var newLine = "";
+
+                for (int i = 0; i < lines[s].Length; i++)
                 {
                     if (!char.IsWhiteSpace(lines[s][i]))
                         newLine += lines[s][i];
@@ -36,27 +47,23 @@ namespace Calculator
             return lines;
         }
 
-        static string[] DivideTheExpression(string[] dividedLine, string[] lines, string[] output)
+        static string[] DivideTheExpression(string[] dividedLine, string[] lines, int i)
         {
-            for (int i = 0; i < lines.Length; i++)
+            var index = 0;
+
+            foreach (char symbol in lines[i])
             {
-                string result = "";
-                var index = 0;
-
-                foreach (char symbol in lines[i])
+                if (!int.TryParse(Convert.ToString(symbol), out int empty))
                 {
-                    if (!int.TryParse(Convert.ToString(symbol), out int empty))
-                    {
-                        index = lines[i].IndexOf(symbol);
-                        dividedLine[1] = Convert.ToString(symbol);
-                    }
+                    index = lines[i].IndexOf(symbol);
+                    dividedLine[1] = Convert.ToString(symbol);
                 }
-                dividedLine[0] = lines[i].Substring(0, index);
-                dividedLine[2] = lines[i].Substring(index + 1);
-
-                output[i] = CalculateTheLine(dividedLine[0],dividedLine[1],dividedLine[2], result);
             }
-            return output;
+
+            dividedLine[0] = lines[i].Substring(0, index);
+            dividedLine[2] = lines[i].Substring(index + 1);
+
+            return dividedLine;
         }
 
         static string CalculateTheLine(string firstNumber, string operation, string secondNumber,string result)
@@ -80,9 +87,14 @@ namespace Calculator
                     else result = "Invalid Operation! // n/0 ! //";
                     break;
                 case "!":
-                    for(int i = 1; i <= int.Parse(firstNumber); i++)
-                        result = Convert.ToString(int.Parse(result)*i);
-                    return Convert.ToString(result);
+                    if (int.Parse(firstNumber) != 0)
+                    {
+                        result = "1";
+                        for (int i = 1; i <= int.Parse(firstNumber); i++)
+                            result = Convert.ToString(int.Parse(result) * i);
+                    }
+                    else result = "1 // 0! = 1 //";
+                    break;
             }
             return result;
         }
